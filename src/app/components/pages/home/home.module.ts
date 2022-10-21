60a58f27ca83bc0c8c6d4329f51e5d5cc3c2ee3e
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-21 21:29:11 +0200

    Home welcome message

--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ContentModule } from '../../content/content.module';
 
 
 
@@ -11,7 +12,8 @@ import { HomeRoutingModule } from './home-routing.module';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ContentModule
   ]
 })
 export class HomeModule { }
