61501efd5ad56e52522317ef8177987d207dc3fb
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-21 20:50:14 +0200

    feature: login button added

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ],
   exports: [
     HeaderComponent
