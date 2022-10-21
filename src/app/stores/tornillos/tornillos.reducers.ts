60ed81c68c0c3f32c117683f612125e170ad110f
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-22 00:01:38 +0200

    feat: implemented tornillos reducer

--- src/app/stores/tornillos/tornillos.reducers.ts
+++ src/app/stores/tornillos/tornillos.reducers.ts
@@ -1,12 +1,17 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { tornilloIdUpdated, tornillosInitialized, tornillosUpdated } from "./tornillos.actions";
 import { TornillosState } from "./tornillos.state";
 
 export const TORNILLOS_FEATURE_KEY = 'tornillos';
 
 export const initialState: TornillosState = {
+  nextId: 0,
   tornillos: []
 };
 
 export const tornilloReducer = createReducer(
-  initialState
+  initialState,
+  on(tornillosInitialized, (_state, { tornillos, nextId }): TornillosState => ({ tornillos, nextId })),
+  on(tornillosUpdated, (state, { tornillos }): TornillosState => ({ ...state, tornillos })),
+  on(tornilloIdUpdated, (state, { nextId }): TornillosState => ({...state, nextId}))
 )
