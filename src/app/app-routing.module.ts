f033527bceb62b538e105606feb17585ce670c2f
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-22 18:13:50 +0200

    fix: Redirect unauthorized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo(['home']);
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'tornillos',
     canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToHome},
     loadChildren: () => import('./components/pages/tornillos/tornillos.module').then(m => m.TornillosModule)
   },
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: 'home'
   },
 ];
 
