ff281fe2d178ce1a628d2eea0cd113ee5e457c75
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-23 18:51:53 +0200

    feat: functional drag & drop

--- src/app/components/dialogs/config-table/config-table.component.ts
+++ src/app/components/dialogs/config-table/config-table.component.ts
@@ -1,12 +1,37 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
+import { TornilloTableColumnDef } from 'src/app/services/tornillos.service';
+import { selectColumnOrder } from 'src/app/stores/tornillos/tornillos.selectors';
 
 @Component({
   selector: 'app-config-table',
   templateUrl: './config-table.component.html',
   styleUrls: ['./config-table.component.scss']
 })
-export class ConfigTableComponent {
+export class ConfigTableComponent implements OnInit {
 
-  constructor() { }
+  columnOrder$ = this.store.select(selectColumnOrder);
+  currentColumnOrder: Map<number, TornilloTableColumnDef> = new Map();
 
+  constructor(private store: Store) { }
+
+  ngOnInit(): void {
+    this.columnOrder$.pipe(take(1)).subscribe((columnOrder) => {
+      columnOrder.forEach((column, index) => {
+        this.currentColumnOrder.set(index, column)
+      })
+    })
+  }
+
+  drop(event: CdkDragDrop<TornilloTableColumnDef[]>) {
+    const auxColumnPrev = this.currentColumnOrder.get(event.previousIndex);
+    const auxColumnCurrent = this.currentColumnOrder.get(event.currentIndex);
+
+    if(auxColumnCurrent && auxColumnPrev) {
+      this.currentColumnOrder.set(event.previousIndex, auxColumnCurrent);
+      this.currentColumnOrder.set(event.currentIndex, auxColumnPrev);
+    }
+  }
 }
--- src/app/components/dialogs/config-table/config-table.module.ts
+++ src/app/components/dialogs/config-table/config-table.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigTableComponent } from './config-table.component';
 import { DragDropModule } from '@angular/cdk/drag-drop'
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,7 +13,9 @@ import { DragDropModule } from '@angular/cdk/drag-drop'
   ],
   imports: [
     CommonModule,
-    DragDropModule
+    DragDropModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     ConfigTableComponent
