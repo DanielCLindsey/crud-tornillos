75d18ea0263ddcb1042755122522f30be4322116
Daniel Camacho Lindsey <camacholindsey.daniel@gmail.com>
2022-10-23 15:20:44 +0200

    Create tornillo

--- src/app/components/dialogs/create-tornillo/create-tornillo.component.ts
+++ src/app/components/dialogs/create-tornillo/create-tornillo.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { createTornillo } from 'src/app/stores/tornillos/tornillos.actions';
 
 @Component({
   selector: 'app-create-tornillo',
@@ -10,13 +12,13 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class CreateTornilloComponent {
 
   createTornillo: FormGroup = new FormGroup({
-    name: new FormControl({ value: '', disabled: false}),
-    price: new FormControl({ value: 0, disabled: false}),
-    format: new FormControl({ value: '', disabled: false}),
-    brand: new FormControl({ value: '', disabled: false})
+    name: new FormControl({ value: '', disabled: false}, [Validators.required]),
+    price: new FormControl({ value: 0, disabled: false}, [Validators.required]),
+    format: new FormControl({ value: '', disabled: false}, [Validators.required]),
+    brand: new FormControl({ value: '', disabled: false}, [Validators.required])
   })
 
-  constructor(public dialogRef: MatDialogRef<CreateTornilloComponent>) {}
+  constructor(public dialogRef: MatDialogRef<CreateTornilloComponent>, private store: Store) {}
 
   decreaseNumber(control: string) {
     const selectedControl = this.createTornillo.get([control]);
@@ -37,5 +39,9 @@ export class CreateTornilloComponent {
     currentValue = (currentValue * 10 + 1)/10;
 
     selectedControl?.setValue(currentValue);
+  }
+
+  submit() {
+    this.store.dispatch(createTornillo({ tornillo: this.createTornillo.value }))
   }
 }
