032990ab09d4ff6fec08ae2c66883fb08cc5b9ea
Daniel Camacho Lindsey <dcamacli@mercadona.es>
2023-03-24 12:01:17 +0100

    feat: config table testing

--- src/app/components/dialogs/config-table/config-table.component.spec.ts
+++ src/app/components/dialogs/config-table/config-table.component.spec.ts
@@ -1,24 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { initialState } from 'src/app/stores/tornillos/tornillos.reducers';
-import { TornillosState } from 'src/app/stores/tornillos/tornillos.state';
-import { cold } from 'jasmine-marbles';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
 
-import { ConfigTableComponent } from './config-table.component';
+import {ConfigTableComponent} from './config-table.component';
+import {TornilloTableColumnDef} from "../../../services/tornillos.service";
+import {selectColumnOrder} from "../../../stores/tornillos/tornillos.selectors";
+import {MatDialogRef} from "@angular/material/dialog";
 
 describe('ConfigTableComponent', () => {
   let component: ConfigTableComponent;
   let fixture: ComponentFixture<ConfigTableComponent>;
-  let store: MockStore<TornillosState>;
+  let store: MockStore;
+
+  const mockColumnOrder: TornilloTableColumnDef[] = [
+    {
+      columnDef: 'mock',
+      header: 'mock',
+      cell: () => `mock`,
+    },
+    {
+      columnDef: 'mock1',
+      header: 'mock1',
+      cell: () => `mock1`,
+    }]
+
+  afterEach(() => {
+    store?.resetSelectors();
+  })
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfigTableComponent ],
+      declarations: [ConfigTableComponent],
       providers: [
-        provideMockStore<TornillosState>({ initialState })
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: () => true
+          }
+        },
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectColumnOrder,
+              value: [
+                {
+                  columnOrder: mockColumnOrder
+                }
+              ]
+            }
+          ]
+        })
       ]
     })
-    .compileComponents();
+      .compileComponents();
 
     store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(ConfigTableComponent);
@@ -30,11 +63,30 @@ describe('ConfigTableComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should load column data', (done) => {
+    store.select(selectColumnOrder).subscribe((columnOrder) => {
+      expect(component.currentColumnOrder.get(0)).toEqual(columnOrder[0])
+      done();
+    })
+  })
+
+  it('should drag and drop updating values', () => {
+    component.currentColumnOrder.set(0, mockColumnOrder[0])
+    component.currentColumnOrder.set(1, mockColumnOrder[1])
+    component.drop(0, 1);
+
+    expect(component.currentColumnOrder.get(0)?.columnDef).toEqual(mockColumnOrder[1].columnDef)
+  })
+
   it('should close', () => {
+    const spy = spyOn(component.dialogRef, 'close').and.callThrough()
     component.close();
+    expect(spy).toHaveBeenCalled();
   })
 
   it('should submit', () => {
+    const spy = spyOn(component.dialogRef, 'close').and.callThrough()
     component.submit();
+    expect(spy).toHaveBeenCalled();
   })
 });
--- src/app/components/dialogs/config-table/config-table.component.ts
+++ src/app/components/dialogs/config-table/config-table.component.ts
@@ -1,11 +1,10 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, OnInit, Optional } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { Store } from '@ngrx/store';
-import { take } from 'rxjs';
-import { TornilloTableColumnDef } from 'src/app/services/tornillos.service';
-import { TornillosActions } from 'src/app/stores/tornillos/tornillos.actions';
-import { selectColumnOrder } from 'src/app/stores/tornillos/tornillos.selectors';
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {Store} from '@ngrx/store';
+import {finalize, take, tap} from 'rxjs';
+import {TornilloTableColumnDef} from 'src/app/services/tornillos.service';
+import {TornillosActions} from 'src/app/stores/tornillos/tornillos.actions';
+import {selectColumnOrder} from 'src/app/stores/tornillos/tornillos.selectors';
 
 @Component({
   selector: 'app-config-table',
@@ -16,33 +15,39 @@ export class ConfigTableComponent implements OnInit {
 
   columnOrder$ = this.store.select(selectColumnOrder);
   currentColumnOrder: Map<number, TornilloTableColumnDef> = new Map();
+  isLoading = false;
 
-  constructor(private store: Store, @Optional() public dialogRef: MatDialogRef<ConfigTableComponent>) { }
+  constructor(private store: Store, public dialogRef: MatDialogRef<ConfigTableComponent>) {
+  }
 
   ngOnInit(): void {
-    this.columnOrder$.pipe(take(1)).subscribe((columnOrder) => {
+    this.columnOrder$.pipe(take(1), tap(() => this.isLoading = true), finalize(() => this.isLoading = false)).subscribe((columnOrder) => {
       columnOrder.forEach((column, index) => {
         this.currentColumnOrder.set(index, column)
       })
     })
   }
 
-  drop(event: CdkDragDrop<TornilloTableColumnDef[]>) {
-    const auxColumnPrev = this.currentColumnOrder.get(event.previousIndex);
-    const auxColumnCurrent = this.currentColumnOrder.get(event.currentIndex);
+  drop(previousIndex: number, currentIndex: number) {
+    const auxColumnPrev = this.currentColumnOrder.get(previousIndex);
+    const auxColumnCurrent = this.currentColumnOrder.get(currentIndex);
 
-    if(auxColumnCurrent && auxColumnPrev) {
-      this.currentColumnOrder.set(event.previousIndex, auxColumnCurrent);
-      this.currentColumnOrder.set(event.currentIndex, auxColumnPrev);
+    if (auxColumnCurrent && auxColumnPrev) {
+      this.currentColumnOrder.set(previousIndex, auxColumnCurrent);
+      this.currentColumnOrder.set(currentIndex, auxColumnPrev);
     }
   }
 
   submit() {
-    this.store.dispatch(TornillosActions.changeColumnOrder({ columnOrder: [...this.currentColumnOrder.values()] }));
+    this.store.dispatch(TornillosActions.changeColumnOrder({columnOrder: [...this.currentColumnOrder.values()]}));
     this.close();
   }
 
   close() {
     this.dialogRef?.close()
+  }
+
+  get currentColumnOrderList() {
+    return [...this.currentColumnOrder.values()];
   }
 }
